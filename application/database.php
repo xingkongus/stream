274fbbf3d0fb677428dc9ed0dd53473c571e48c7
Hansin <845612500@qq.com>
2018-07-15 11:16:54 +0800

    隐藏密码

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'stream',
+    'database'        => '******',
     // 用户名
-    'username'        => 'stream',
+    'username'        => '******',
     // 密码
-    'password'        => 'xingkongstreambest',
+    'password'        => '******',
     // 端口
     'hostport'        => '',
     // 连接dsn
